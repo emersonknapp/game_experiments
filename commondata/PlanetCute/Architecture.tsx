7ec6fe4ca0ec038d9c7fcf99068f797d5e682997
CandleEnds <emerson.b.knapp@gmail.com>
2013-06-02 16:29:22 -0600

    Arranged planetcute tiles into usable states.

---
+++ commondata/PlanetCute/Architecture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Architecture" tilewidth="101" tileheight="171">
+ <image source="Architecture/montage.png" trans="ffffff" width="303" height="855"/>
+</tileset>
